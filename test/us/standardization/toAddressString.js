83618767a27588af42c43e1bd5bdfdf4e6c10b58
Michael Pelikan <michael@pelikan.org>
2015-08-25 13:55:56 -0600

    Added more toAddressString tests.

--- test/us/standardization/toAddressString.js
+++ test/us/standardization/toAddressString.js
@@ -4,7 +4,7 @@
 
 var should = require( "should" );
 
-// var logStatus = require( "../../../lib/common/logStatus" );
+var logStatus = require( "../../../lib/common/logStatus" );
 
 var Postal = require( "../../../lib" ).USPostal;
 
@@ -15,23 +15,77 @@ describe( "To Address String:", function () {
 		postal = new Postal();
 	}
 
-	it( "16729 Buckskin Way, Broomfield, CO 80023", function () {
+	function verifyAddress( addressString, debugLogging ) {
+		if ( debugLogging ) {
+			logStatus( true );
+		}
+
 		if ( !postal ) {
 			return;
 		}
+		if ( !addressString ) {
+			return;
+		}
 
-		var addressObject = {
-			number: "16729",
-			street: "BUCKSKIN",
-			type: "WAY",
-			city: "BROOMFIELD",
-			state: "CO",
-			zip: "80023"
-		};
-		var addressStringExp = "16729 BUCKSKIN WAY, BROOMFIELD, CO 80023";
-		var addressStringTest = postal.standardization.toAddressString( addressObject );
-		should.exist( addressStringTest );
-		addressStringTest.should.equal( addressStringExp );
+		postal.standardization.parseAddress( addressString, function ( err, addressObject ) {
+			var generatedAddressString;
+
+			should.not.exist( err );
+			should.exist( addressObject );
+
+			generatedAddressString = postal.standardization.toAddressString( addressObject );
+			should.exist( generatedAddressString );
+
+			generatedAddressString.should.equal( addressString );
+
+			if ( debugLogging ) {
+				logStatus( false );
+			}
+		} );
+	}
+
+	it( "16729 BUCKSKIN WAY, BROOMFIELD, CO 80021", function () {
+		verifyAddress( this.test.title );
+	} );
+
+	it( "5023 W 120TH AVE STE 277, BROOMFIELD, CO 80021", function () {
+		verifyAddress( this.test.title );
+	} );
+
+	it( "5023 W 120TH AVE # 277, BROOMFIELD, CO 80021", function () {
+		verifyAddress( this.test.title );
+	} );
+
+	it( "5023 W 120TH AVE # 277, BROOMFIELD, CO 80021-5606", function () {
+		verifyAddress( this.test.title );
+	} );
+
+	it( "5023 W 120TH AVE # 277, BROOMFIELD, CO 80021-5606, UNITED STATES", function () {
+		verifyAddress( this.test.title );
+	} );
+
+	it( "SAN FRANCISCO, CA", function () {
+		verifyAddress( this.test.title );
+	} );
+
+	it( "MISSION & VALENCIA, SAN FRANCISCO, CA", function () {
+		verifyAddress( this.test.title );
+	} );
+
+	it( "MISSION ST & VALENCIA, SAN FRANCISCO, CA", function () {
+		verifyAddress( this.test.title );
+	} );
+
+	it( "MISSION ST & VALENCIA AVE, SAN FRANCISCO, CA", function () {
+		verifyAddress( this.test.title );
+	} );
+
+	it( "MISSION & VALENCIA VTS, SAN FRANCISCO, CA", function () {
+		verifyAddress( this.test.title );
+	} );
+
+	it( "RR 2 BOX 274, LAKE BUTLER, FL 32054", function () {
+		verifyAddress( this.test.title );
 	} );
 
 } );
