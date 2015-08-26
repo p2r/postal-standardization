d65b11a0660420cfc600dd1174731d84ad44dc94
Michael Pelikan <michael@pelikan.org>
2015-08-26 13:59:19 -0600

    Tweaking confidence levels for matching of US addresses.

--- test/us/match/match.js
+++ test/us/match/match.js
@@ -84,7 +84,6 @@ describe( "US Address Match Similarity:", function () {
 		}
 
 		var address1String = "16729 Buckskin Way, Broomfield, CO 80023";
-		var address2String = "16729 Buckskin Way, Broomfield, CO 80023";
 		var address1Object, address2Object;
 
 		postal.standardization.parseAddress( address1String, function ( err, result ) {
@@ -93,7 +92,7 @@ describe( "US Address Match Similarity:", function () {
 
 			address1Object = result;
 
-			postal.standardization.parseAddress( address2String, function ( err, result ) {
+			postal.standardization.parseAddress( address1String, function ( err, result ) {
 				should.not.exist( err );
 				should.exist( result );
 
@@ -134,9 +133,40 @@ describe( "US Address Match Similarity:", function () {
 					should.not.exist( err );
 					should.exist( result );
 
-					// result.should.equal( 0.75 );
-					result.should.be.above( 0.7 );
-					result.should.be.below( 0.8 );
+					result.should.be.above( 0.8 );
+					result.should.be.below( 0.9 );
+				} );
+			} );
+		} );
+	} );
+
+	it( "Strings - Similar, Different Unit Type", function () {
+		if ( !postal ) {
+			return;
+		}
+
+		var address1String = "16729 Buckskin Way #123, Broomfield, CO 80023";
+		var address2String = "16729 Buckskin Way Suite 123, Broomfield, CO 80023";
+		var address1Object, address2Object;
+
+		postal.standardization.parseAddress( address1String, function ( err, result ) {
+			should.not.exist( err );
+			should.exist( result );
+
+			address1Object = result;
+
+			postal.standardization.parseAddress( address2String, function ( err, result ) {
+				should.not.exist( err );
+				should.exist( result );
+
+				address2Object = result;
+
+				postal.match.similarity( address1Object, address2Object, function ( err, result ) {
+					should.not.exist( err );
+					should.exist( result );
+
+					result.should.be.above( 0.9 );
+					result.should.be.below( 1.0 );
 				} );
 			} );
 		} );
